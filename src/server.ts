72e713d4fcf1e2ab42553fd3b2dd53fe2eddc02c
mukhlisa03 <mukhlisamakhmaraimova03@gmail.com>
2025-03-07 23:49:05 +0900

    feat: integration of environmenttal variables

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,16 @@
 console.log("EXECUTED!");
 
-import moment from "moment";
+// import moment from "moment";
 
-const currentTime = moment().format("YYYY MM DD");
-console.log(currentTime);
+// const currentTime = moment().format("YYYY MM DD");
+// console.log(currentTime);
 
-const person: string = "Mukhlisa";
-const count: number = 100; 
+// const person: string = "Mukhlisa";
+// const count: number = 100; 
+
+
+import dotenv from "dotenv"; 
+dotenv.config(); 
+
+console.log("PORT:", process.env.PORT);
+console.log("MONGO_URL:", process.env.MONGO_URL);
