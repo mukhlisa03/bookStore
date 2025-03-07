e03122e2707512250fae58ee9b95edcb844eae0b
mukhlisa03 <mukhlisamakhmaraimova03@gmail.com>
2025-03-08 00:24:59 +0900

    feat: express integrations

--- src/server.ts
+++ src/server.ts
@@ -8,18 +8,20 @@ console.log("EXECUTED!");
 // const count: number = 100; 
 
 
+
 import dotenv from "dotenv"; 
 dotenv.config(); 
+import app from "./app";
 
 import mongoose from "mongoose";
 mongoose
   .connect(process.env.MONGO_URL as string, {})
   .then((data) => {
     console.log("MongoDB connection succeed!");
-    const PORT = process.env.PORT ?? 3003; 
-    // app.listen(PORT, function () {
-    //   console.info(`The server is running successfully on port: ${PORT}`);
-    //   console.info(`Admin project on http://localhost:${PORT}/admin \n`); // Admin projectning homepage ga yuboradi
-    // });
+    const PORT = process.env.PORT ?? 2003; 
+    app.listen(PORT, function () {
+      console.info(`The server is running successfully on port: ${PORT}`);
+      console.info(`Admin project on http://localhost:${PORT}/admin \n`); // Admin projectning homepage ga yuboradi
+    });
   })
   .catch((err) => console.log("ERROR on connection MongoDB", err));
