53b69dc5cfbefe687b81e185c871cdeb118bbd0a
mukhlisa03 <mukhlisamakhmaraimova03@gmail.com>
2025-03-08 00:03:02 +0900

    feat: connect mongodb via mongoose

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,6 @@
 console.log("EXECUTED!");
 
 // import moment from "moment";
-
 // const currentTime = moment().format("YYYY MM DD");
 // console.log(currentTime);
 
@@ -12,5 +11,15 @@ console.log("EXECUTED!");
 import dotenv from "dotenv"; 
 dotenv.config(); 
 
-console.log("PORT:", process.env.PORT);
-console.log("MONGO_URL:", process.env.MONGO_URL);
+import mongoose from "mongoose";
+mongoose
+  .connect(process.env.MONGO_URL as string, {})
+  .then((data) => {
+    console.log("MongoDB connection succeed!");
+    const PORT = process.env.PORT ?? 3003; 
+    // app.listen(PORT, function () {
+    //   console.info(`The server is running successfully on port: ${PORT}`);
+    //   console.info(`Admin project on http://localhost:${PORT}/admin \n`); // Admin projectning homepage ga yuboradi
+    // });
+  })
+  .catch((err) => console.log("ERROR on connection MongoDB", err));
