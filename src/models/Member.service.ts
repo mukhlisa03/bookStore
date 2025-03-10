54b56041af93d8cf3b4f0b690fe356997c5b0d88
mukhlisa03 <mukhlisamakhmaraimova03@gmail.com>
2025-03-11 04:08:22 +0900

    fix: modify library login and signup process

--- src/models/Member.service.ts
+++ src/models/Member.service.ts
@@ -2,6 +2,7 @@ import MemberModel from "../schema/Member.model";
 import { LoginInput, Member, MemberInput } from "../libs/types/member";
 import Errors, { HttpCode, Message } from "../libs/Error";
 import { MemberType } from "../libs/enums/member.enum";
+import * as bcrypt from "bcryptjs";
 
 class MemberService {
   private readonly memberModel;
@@ -16,6 +17,12 @@ class MemberService {
       .exec();
     console.log("exist:", exist);
     if (exist) throw new Errors(HttpCode.BAD_REQUEST, Message.CREATE_FAILED);
+
+    console.log("before:", input.memberPassword);
+    const salt = await bcrypt.genSalt();
+    input.memberPassword = await bcrypt.hash(input.memberPassword, salt);
+    console.log("after:", input.memberPassword);
+
     try {
       const result = await this.memberModel.create(input);
       result.memberPassword = "";
@@ -27,14 +34,6 @@ class MemberService {
     }
   }
 
-
-
-  // public async processLogin(input: LoginInput): Promise<Member> {
-  //   const member = await this.memberModel.findOne({memberNick: input.memberNick}).exec();
-  //   console.log("member:", member);
-  //   return member.toObject() as Member;
-    
-  // }
 
 
   public async processLogin(input: LoginInput): Promise<Member> {
@@ -49,7 +48,11 @@ class MemberService {
         throw new Errors(HttpCode.NOT_FOUND, Message.NO_MEMBER_NICK);
     }
 
-     const isMatch = input.memberPassword === member.memberPassword;
+    const isMatch = await bcrypt.compare(
+      input.memberPassword,
+      member.memberPassword
+    );
+
 
      if(!isMatch) {
       throw new Errors(HttpCode.UNAUTHORIZED, Message.WRONG_PASSWORD);
