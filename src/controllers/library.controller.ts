58195c06744424367e137a65d67b57463d68b499
mukhlisa03 <mukhlisamakhmaraimova03@gmail.com>
2025-03-13 03:26:48 +0900

    fix: create sessions on login and signup process

--- src/controllers/library.controller.ts
+++ src/controllers/library.controller.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 import { T } from "../libs/types/common";
 import MemberService from "../models/Member.service";
-import { LoginInput, MemberInput } from "../libs/types/member";
+import { AdminRequest, LoginInput, MemberInput } from "../libs/types/member";
 import { MemberType } from "../libs/enums/member.enum";
 
 const memberService = new MemberService();
@@ -35,31 +35,38 @@ libraryController.getLogin = (req: Request, res: Response) => {
 };
 
 
-libraryController.processSignup = async (req: Request, res: Response) => {
+libraryController.processSignup = async (req: AdminRequest, res: Response) => {
   try {
     console.log("processSignup");
 
     const newMember: MemberInput = req.body;
     newMember.memberType = MemberType.LIBRARY;
     const result = await memberService.processSignup(newMember);
-    // TODO: SESSIONS Authentication
 
-    res.send(result);
+    req.session.member = result;  // sesion collectionning member qsmiga resultda yangi hosil bolgan libraryni qoshadi
+    req.session.save(function () {
+      res.send(result);
+    });
+
   } catch (err) {
     console.log("Error, processSignup", err);
     res.send(err);
   }
 };
 
-libraryController.processLogin = async (req: Request, res: Response) => {
+
+libraryController.processLogin = async (req: AdminRequest, res: Response) => {
   try {
     console.log("processLogin");
 
     const input: LoginInput = req.body;
     const result = await memberService.processLogin(input);
-    // TODO: SESSIONS Authentication
 
-    res.send(result);
+    req.session.member = result;  // sesion collectionning member qsmiga resultda yangi hosil bolgan libraryni qoshadi
+    req.session.save(function () {
+      res.send(result);
+    });
+
   } catch (err) {
     console.log("Error, processLogin", err);
     res.send(err);
--- src/libs/types/member.ts
+++ src/libs/types/member.ts
@@ -1,5 +1,8 @@
 import { ObjectId } from "mongoose"; 
 import { MemberStatus, MemberType } from "../enums/member.enum";
+import { Request } from "express";
+import { Session } from "express-session";
+
 
 export interface Member {
     _id: ObjectId;
@@ -31,4 +34,12 @@ export interface MemberInput {
 export interface LoginInput {
     memberNick: string;
     memberPassword: string;
+}
+
+
+export interface AdminRequest extends Request {
+    member: Member;   // member qiymat => type yuqoridagi Memberga teng
+    session: Session & {member: Member};  // type Session (ichida => member bor)
+    file: Express.Multer.File;
+    files: Express.Multer.File[];
 }
