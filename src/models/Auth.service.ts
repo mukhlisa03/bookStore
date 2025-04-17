67ffa6b43369fa279e2f60205ab74d42df7b8a23
mukhlisa03 <mukhlisamakhmaraimova03@gmail.com>
2025-04-17 20:05:43 +0900

    feat: develop verify authenticated user credentials

--- src/models/Auth.service.ts
+++ src/models/Auth.service.ts
@@ -3,9 +3,11 @@ import { AUTH_TIMER } from "../libs/config";
 import { Member } from "../libs/types/member";
 import jwt from "jsonwebtoken";
 
-
 class AuthService {
-  constructor() {}
+  private readonly secretToken;
+  constructor() {
+    this.secretToken = process.env.SECRET_TOKEN as string;
+  }
 
   public async createToken(payload: Member) {
     return new Promise((resolve, reject) => {
@@ -25,6 +27,15 @@ class AuthService {
         }
       );
     });
+  }
+
+  public async checkAuth(token: string): Promise<Member> {
+    const result: Member = (await jwt.verify(
+      token,
+      this.secretToken
+    )) as Member;
+    console.log(`--- [AUTH] memberNick: ${result.memberNick} ---`);
+    return result;
   }
 }
 
