0767401ca1f46da5e46cd3f12cb7079ec8941463
mukhlisa03 <mukhlisamakhmaraimova03@gmail.com>
2025-04-17 19:35:57 +0900

    feat: develop authentication via tokens with cookies

--- src/controllers/member.controller.ts
+++ src/controllers/member.controller.ts
@@ -1,10 +1,11 @@
-import {Request, Response} from 'express';
+import { Request, Response } from "express";
 import { T } from "../libs/types/common";
-import MemberService from '../models/Member.service';
-import { LoginInput, Member, MemberInput } from '../libs/types/member';
-import Errors from '../libs/Error';
-import libraryController from './library.controller';
-import AuthService from '../models/Auth.service';
+import MemberService from "../models/Member.service";
+import { LoginInput, Member, MemberInput } from "../libs/types/member";
+import Errors, { HttpCode } from "../libs/Error";
+import libraryController from "./library.controller";
+import AuthService from "../models/Auth.service";
+import { AUTH_TIMER } from "../libs/config";
 
 // REACT
 
@@ -13,39 +14,44 @@ const authService = new AuthService();
 
 const memberController: T = {};
 
-
 memberController.signup = async (req: Request, res: Response) => {
   try {
     console.log("signup");
     const input: MemberInput = req.body,
-     result: Member = await memberService.signup(input); 
-     const token = await authService.createToken(result);
-     console.log("token:", token);
+      result: Member = await memberService.signup(input);
+    const token = await authService.createToken(result);
+    // console.log("token:", token);
 
-     console.log("result:", result);
-    
-    res.json({member: result});
+    res.cookie("accessToken", token, {
+      maxAge: AUTH_TIMER * 3600 * 1000,
+      httpOnly: false,
+    });
+
+    res.status(HttpCode.CREATED).json({ member: result, accessToken: token });
   } catch (err) {
     console.log("Error, signup", err);
-    if(err instanceof Errors) res.status(err.code).json(err);
+    if (err instanceof Errors) res.status(err.code).json(err);
     else res.status(Errors.standard.code).json(Errors.standard);
   }
 };
-
 
 memberController.login = async (req: Request, res: Response) => {
   try {
     console.log("login");
     const input: LoginInput = req.body,
-    result = await memberService.login(input),
-    token = await authService.createToken(result);
-    console.log("token: ", token);
+      result = await memberService.login(input),
+      token = await authService.createToken(result);
+    // console.log("token: ", token);
 
+    res.cookie("accessToken", token, {
+      maxAge: AUTH_TIMER * 3600 * 1000,
+      httpOnly: false,
+    });
 
-    res.json({member: result});
+    res.status(HttpCode.OK).json({ member: result, accessToken: token });
   } catch (err) {
     console.log("Error, login", err);
-    if(err instanceof Errors) res.status(err.code).json(err);
+    if (err instanceof Errors) res.status(err.code).json(err);
     else res.status(Errors.standard.code).json(Errors.standard);
   }
 };
